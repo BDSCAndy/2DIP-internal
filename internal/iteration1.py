2722459b335e323d9dcaef285179fd92c279bdb3
BDSCAndy <129335695+BDSCAndy@users.noreply.github.com>
2023-04-05 09:23:19 +1200

    Delete iteration1.py

--- internal/iteration1.py
+++
@@ -1,3 +0,0 @@
-MENU={"Hot lunches:":"\n---------------","Hot noodles(Halal,Beef & Spicy":3.80,"Spaghetti Bun":1.50,"Garlic Bread":2.00,"Hot Dogs with Tomato/Mustard sauce":4.00,"-----------------------------":"Healthy Choices"}
-for item,price in MENU.items():
-    print(item,price)
